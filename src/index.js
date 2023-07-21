a3ebcacc148dc2cb7c165dda8430f814e3ffb3c3
Ramazanov Vitalii <zapupenec@gmail.com>
2023-07-21 15:24:56 +0300

    task-done

--- src/index.js
+++ src/index.js
@@ -1,18 +1,66 @@
-const button = document.getElementById('button-show-search');
-button.addEventListener('click', () => {
-  const sideBarSearchEl = document.querySelector('.side-bar-search');
-  sideBarSearchEl.classList.remove('hidden');
-  sideBarSearchEl.classList.add('shown');
-});
+const renderSearchPanel = (elements, state) => {
+  const mapping = {
+    hidden: (elements) => {
+      elements.searchPanelEl.classList.add('hidden');
+      elements.searchPanelEl.classList.remove('shown');
+    },
+    shown: (elements) => {
+      elements.searchPanelEl.classList.remove('hidden');
+      elements.searchPanelEl.classList.add('shown');
+    },
+  };
+  return mapping[state.searchPanelState](elements);
+};
 
-const canselButton = document.getElementById('button-hide-search');
-canselButton.addEventListener('click', () => {
-  const sideBarSearchEl = document.querySelector('.side-bar-search');
-  sideBarSearchEl.classList.remove('shown');
-  sideBarSearchEl.classList.add('hidden');
-});
+const renderTabs = (elements, state) => {
+  elements.tabList.forEach((tab) => {
+    const tabpanel = document.querySelector(`[aria-labelledby="${tab.id}"]`);
+    if (state.activeTabId === tab.id) {
+      tab.classList.add('active-switcher');
+      tab.classList.remove('inactive-switcher');
+      tabpanel.classList.remove('carusel-inactive');
+    } else {
+      tab.classList.remove('active-switcher');
+      tab.classList.add('inactive-switcher');
+      tabpanel.classList.add('carusel-inactive');
+    }
+  });
+};
 
-const formSearch = document.getElementById('search-form');
-formSearch.addEventListener('submit', (e) => {
-  e.preventDefault();
-});
+const app = () => {
+  const state = {
+    searchPanelState: 'hidden',
+    activeTabId: 'carusel-days'
+  }
+
+  const elements = {
+    btnShowSearch: document.getElementById('button-show-search'),
+    btnHideSearch: document.getElementById('button-hide-search'),
+    searchPanelEl: document.querySelector('.side-bar-search'),
+    formSearchEl: document.getElementById('search-form'),
+    tabList: document.querySelectorAll('[role="tab"]'),
+  }
+
+  elements.tabList.forEach((tab) => {
+    tab.addEventListener('click', (e) => {
+      state.activeTabId = e.target.id;
+      renderTabs(elements, state);
+    })
+  })
+
+  elements.btnShowSearch.addEventListener('click', () => {
+    state.searchPanelState = 'shown';
+    renderSearchPanel(elements, state);
+  });
+
+  elements.btnHideSearch.addEventListener('click', () => {
+    state.searchPanelState = 'hidden';
+    renderSearchPanel(elements, state);
+  });
+
+  elements.formSearchEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+  });
+};
+
+app();
